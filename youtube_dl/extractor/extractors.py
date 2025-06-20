6a0a6693c9ad78bca559744d61aba59dadd656cf
Connor Mason <--local>
2025-06-19 23:58:54 -0700

    Remove more extractors

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1,33 +1,10 @@
 # flake8: noqa
 from __future__ import unicode_literals
 
-from .appleconnect import AppleConnectIE
-from .applepodcasts import ApplePodcastsIE
-from .bandcamp import BandcampIE, BandcampAlbumIE, BandcampWeeklyIE
-from .beatport import BeatportIE
-from .cloudflarestream import CloudflareStreamIE
-from .deezer import DeezerPlaylistIE
-from .dropbox import DropboxIE
-from .eighttracks import EightTracksIE
-from .googledrive import GoogleDriveIE
-from .googlepodcasts import (
-    GooglePodcastsIE,
-    GooglePodcastsFeedIE,
-)
-from .googlesearch import GoogleSearchIE
-from .iheart import (
-    IHeartRadioIE,
-    IHeartRadioPodcastIE,
-)
 from .mixcloud import (
     MixcloudIE,
     MixcloudUserIE,
     MixcloudPlaylistIE,
-)
-from .safari import (
-    SafariIE,
-    SafariApiIE,
-    SafariCourseIE,
 )
 from .soundcloud import (
     SoundcloudEmbedIE,
@@ -38,31 +15,9 @@ from .soundcloud import (
     SoundcloudPlaylistIE,
     SoundcloudSearchIE,
 )
-from .soundgasm import SoundgasmIE, SoundgasmProfileIE
 from .spotify import (
     SpotifyIE,
     SpotifyShowIE,
-)
-from .streamcloud import StreamcloudIE
-from .tunein import (
-    TuneInClipIE,
-    TuneInStationIE,
-    TuneInProgramIE,
-    TuneInTopicIE,
-    TuneInShortenerIE,
-)
-from .twitch import (
-    TwitchVodIE,
-    TwitchCollectionIE,
-    TwitchVideosIE,
-    TwitchVideosClipsIE,
-    TwitchVideosCollectionsIE,
-    TwitchStreamIE,
-    TwitchClipsIE,
-)
-from .vevo import (
-    VevoIE,
-    VevoPlaylistIE,
 )
 from .youtube import (
     YoutubeIE,
